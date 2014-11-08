e413df7f23a92f20b9dcd3b33d548da61c91ecc3
concreted <arichuang@gmail.com>
2014-11-07 16:15:32 -0800

    attempt slide with jquery

--- chrome/background.js
+++ chrome/background.js
@@ -1,1 +1,9 @@
 console.log('background.js');
+
+// $(document).ready(function(){
+//     alert("working");
+// });
+
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+  console.log('updated');
+});
--- chrome/content.js
+++ chrome/content.js
@@ -1,1 +1,24 @@
 console.log('content.js');
+
+var toggler = '<li id="gd-toggle"><a href="#">Giraffe Draft</a></li>';
+
+var sidebar = '<div class="gd-sidebar from-left">asdf</div>';
+
+// Add styles
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('styles.css');
+(document.head||document.documentElement).appendChild(style);
+
+console.log($('#yucs-top-list'));
+$('#yucs-top-list').prepend(toggler);
+
+$('body').prepend(sidebar);
+
+$('#gd-toggle').click(function() {
+  //alert('hello!');
+  console.log($('.gd-sidebar'));
+  $('.gd-sidebar').toggleClass('is-visible');
+  console.log($('.gd-sidebar.is-visible'));
+});
