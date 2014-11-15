4df4f6cedade6b8eb1c7868df900b488b268d15a
concreted <arichuang@gmail.com>
2014-11-15 15:36:15 -0800

    Scraping data from Yahoo and sending to iframe

---
+++ chrome/content/content.js
@@ -0,0 +1,190 @@
+// Insert iFrame referencing popup.html
+var iframe = document.createElement('iframe');
+iframe.src = chrome.extension.getURL("popup.html");
+iframe.id = 'giraffedraft';
+iframe.align = 'right';
+iframe.width = '300';
+iframe.height = '100%';
+iframe.style.position = "fixed";
+iframe.style.top = '0px';
+iframe.style.right = '0px';
+iframe.style.zIndex = '10000000000';
+iframe.style.backgroundColor = 'white';
+document.body.appendChild(iframe);
+
+
+var undrafted = [];
+var suggestions = [];
+var drafted = [];
+var state = {};
+var allStats = {};
+
+var sendState = function() {
+  var w = document.querySelector('#giraffedraft').contentWindow;
+  w.postMessage({state: state}, '*');
+};
+
+var sendUser = function() {
+  var w = document.querySelector('#giraffedraft').contentWindow;
+  // not working, why?
+  //var user = document.querySelector('#fixed-pick').querySelector('.Ell').innerText;
+  var user = 'Walter';
+  console.log("sending user:", user);
+  w.postMessage({user: user}, '*');
+};
+
+var click = function(){
+  document.querySelector('.NavTabs').childNodes[5].click();
+};
+
+// var getUserName = function() {
+//   document.querySelector('#fixed-pick').querySelector('.Ell').innerText;
+// };
+
+var getPlayers = function() {
+  document.querySelector('.NavTabs').childNodes[5].click();
+  document.querySelector('.SubNavTabs').children[1].click();
+
+  var players = document.getElementsByClassName('Fz-xs Ell');
+  //console.log(players);
+  Array.prototype.slice.call(players).forEach(function(player) {
+    state[player.innerHTML] = {};
+  });
+};
+
+var selectDraftResults = function() {
+  // Select the draft results tab
+  document.querySelector('.NavTabs').childNodes[5].click();
+  document.querySelector('.SubNavTabs').children[0].click();
+};
+
+var updateState = function() {
+  // selectDraftResults();
+  // // Drafted player
+  // var draftedPlayer = document.querySelector('#results-by-round').querySelector('tbody').children[1].children[1].innerText;
+  // // Fantasy sports player
+  // var fantasyPlayer = document.querySelector('#results-by-round').querySelector('tbody').children[1].children[2].innerText.trim();
+  //
+  // state[fantasyPlayer].push(draftedPlayer);
+  //
+  // //console.log(state);
+  // sendState();
+};
+
+var getPlayerStats = function() {
+  // select player tab
+  document.querySelector('.NavTabs').childNodes[1].click();
+
+  // click 'show drafted'
+  // should check if already clicked.
+  document.querySelector('#show-drafted').click();
+
+
+  // get players list
+  var players = document.querySelector('.player-listing-table').querySelector('tbody').children;
+
+  // get stat categories
+  var statCategoriesNode = document.querySelector('.player-listing-table').querySelector('thead').children[0].children;
+
+  var statCategories = [];
+  for (var i = 2; i < statCategoriesNode.length-1; i++) {
+    //debugger;
+    var stat = statCategoriesNode[i].querySelector('div').innerText;
+    statCategories.push(stat);
+  }
+
+  console.log(statCategories);
+
+  // fill out allStats table
+  Array.prototype.slice.call(players).forEach(function(player) {
+    //console.log(player);
+
+    // get player name
+    var playerName = player.children[1].innerText.slice(3);
+    var stats = {};
+    // first index is ID
+    // second index is player name
+    // last index is extra td
+    for (var i = 2; i < player.children.length-1; i++) {
+      stats[statCategories[i-2]] = player.children[i].innerText;
+    }
+    allStats[playerName] = stats;
+  });
+
+  console.log(allStats);
+  console.log(allStats.length);
+
+};
+
+var initialize = function() {
+  // Populates fantasy players into state
+  getPlayers();
+  //console.log(state);
+
+  selectDraftResults();
+
+  var draft = document.querySelector('#results-by-round').querySelector('tbody').children;
+  Array.prototype.slice.call(draft).forEach(function(playerNode) {
+    if (playerNode.className !== 'drkTheme') {
+      // grab stats:
+      // click on player
+      playerNode.children[1].click();
+      //debugger;
+      // read his stats
+
+      var stats = {};
+      var categoriesNode = document.querySelector('.ys-playerdetails-table').querySelector('thead').querySelector('tr').children;
+      var statsNode = document.querySelector('.ys-playerdetails-table').querySelector('tbody').querySelector('tr').children;
+
+      var categoriesList = Array.prototype.slice.call(categoriesNode);
+      var statsList = Array.prototype.slice.call(statsNode);
+
+      // start from second index - first index is description
+      for (var i = 1; i < categoriesList.length; i++) {
+        stats[categoriesList[i].innerText] = statsList[i].innerText;
+      }
+
+      //console.log(playerNode.children[1].innerText);
+      //console.log(stats);
+
+      var fantasyPlayer = playerNode.children[2].innerText.trim();        // Need to trim because of leading space before each player's name
+      //console.log('Fantasy player:', fantasyPlayer);
+      var draftedPlayer = playerNode.children[1].innerText;
+      state[fantasyPlayer][draftedPlayer] = stats;
+    }
+  });
+  console.log(state);
+
+  // Set event listener to scrape the DOM
+  document.querySelector('.Col2c').addEventListener('DOMNodeInserted', updateState);
+};
+
+// from smack talk
+// Get the player's name
+// Get the teams
+
+window.addEventListener("message", receiveMessage, false);
+function receiveMessage(event) {
+  console.log("=======================message received in content.js!======================");
+  console.log(event.data);
+  if (event.data.command === 'init') {
+    console.log('initializing');
+    initialize();
+  }
+  if (event.data.command === 'sync') {
+    initialize();
+    console.log('sending state');
+    console.log(JSON.stringify(state));
+    sendUser();
+    sendState();
+  }
+  if (event.data.command === 'players') {
+    getPlayerStats();
+  }
+}
+
+
+// setInterval(function() {
+//   console.log(JSON.stringify(state));
+//   sendState();
+// }, 2000);
--- chrome/slider.js
+++ chrome/slider.js
@@ -4,19 +4,68 @@ angular.module('gDPopup', ['gDraft.services', 'angular-c3'])
   $scope.undrafted = [];
   $scope.suggestions = [];
   $scope.drafted = [];
+  $scope.user = '';
+  $scope.state = {};
 
+  $scope.leagueAverages = {
+    // "TO":
+    // "BLK":
+    // "ST":
+    // "AST":
+    // "REB":
+    // "PTS":
+    // "3PTM"
+    // "FT%":
+    // "FTA":
+    // "FTM":
+    // "FG%":
+    // "FGA":
+    // "FGM":
+
+    "MIN":48*82,
+    "3PM":7.5*82,
+    "FGM":0.5,
+    "BLK":4.8*82,
+    "STL":7.2*82,
+    "AST":21.2*82,
+    "GP":82,
+    "REB":42.1*82,
+    "FT%":0.5,
+    "PTS":99.4*82,
+    "TO":14.9*82
+  };
+
+  // corrected to match yahoo
   $scope.teamStats = {
-    "MIN":20,
-    "3PM":100,
-    "FG%":30,
-    "BLK":50,
-    "STL":40,
-    "AST":60,
-    "GP":70,
-    "REB":80,
-    "FT%":90,
-    "PTS":20,
-    "TO":20
+    "FGM": 0,
+    "FGA": 0,
+    "FG%": 0,
+    "FTM": 0,
+    "FTA": 0,
+    "FT%": 0,
+    "3PTM": 0,
+    "PTS": 0,
+    "REB": 0,
+    "AST": 0,
+    "ST": 0,
+    "BLK": 0,
+    "TO": 0,
+  };
+
+  $scope.playerStats = {
+    "FGM": 0,
+    "FGA": 0,
+    "FG%": 0,
+    "FTM": 0,
+    "FTA": 0,
+    "FT%": 0,
+    "3PTM": 0,
+    "PTS": 0,
+    "REB": 0,
+    "AST": 0,
+    "ST": 0,
+    "BLK": 0,
+    "TO": 0,
   };
 
   $scope.config = {
@@ -24,30 +73,59 @@ angular.module('gDPopup', ['gDraft.services', 'angular-c3'])
       columns: [
         [
           'teamStats',
-          $scope.teamStats.MIN,
-          $scope.teamStats['3PM'],
-          $scope.teamStats['FG%'],
-          $scope.teamStats.BLK,
-          $scope.teamStats.STL,
-          $scope.teamStats.AST,
-          $scope.teamStats.GP,
-          $scope.teamStats.REB,
+          $scope.teamStats['FGM'],
+          $scope.teamStats['FGA'],
+          $scope.teamStats["FG%"],
+          $scope.teamStats['FTM'],
+          $scope.teamStats['FTA'],
           $scope.teamStats['FT%'],
-          $scope.teamStats.PTS,
-          $scope.teamStats.TO
+          $scope.teamStats['3PTM'],
+          $scope.teamStats['PTS'],
+          $scope.teamStats['REB'],
+          $scope.teamStats['AST'],
+          $scope.teamStats['ST'],
+          $scope.teamStats['BLK'],
+          $scope.teamStats['TO'],
+        ],
+        [
+          'playerStats',
+          $scope.playerStats['FGM'],
+          $scope.playerStats['FGA'],
+          $scope.playerStats["FG%"],
+          $scope.playerStats['FTM'],
+          $scope.playerStats['FTA'],
+          $scope.playerStats['FT%'],
+          $scope.playerStats['3PTM'],
+          $scope.playerStats['PTS'],
+          $scope.playerStats['REB'],
+          $scope.playerStats['AST'],
+          $scope.playerStats['ST'],
+          $scope.playerStats['BLK'],
+          $scope.playerStats['TO'],
         ]
       ],
       type: 'bar',
       groups: [
-        []
+        ['playerStats', 'teamStats']
       ]
     },
     axis: {
-      rotated: true
+      x: {
+        height:60,
+        max: 11,
+        type: 'category',
+        tick:{
+          rotate:90
+        },
+        categories: ['FGM', 'FGA',	'FG%', 'FTM',	'FTA', 'FT%', '3PTM',	'PTS',	'REB',	'AST',	'ST',	'BLK',	'TO']
+      },
+      y: {
+        max: 3000
+      }
     },
     size: {
       height: '400',
-      width: '400'
+      width: '275'
     }
 
   };
@@ -68,21 +146,21 @@ angular.module('gDPopup', ['gDraft.services', 'angular-c3'])
   .then(function(data){
     console.log(data);
     $scope.undrafted = data;
-    $scope.calculate();
+    // $scope.calculate();
   });
 
-  $scope.calculate = function(){
-    services.getSuggestions($scope.undrafted)
-      .then(function(data){
-        $scope.suggestions = data;
-      });
-  };
+  // $scope.calculate = function(){
+  //   services.getSuggestions($scope.undrafted)
+  //     .then(function(data){
+  //       $scope.suggestions = data;
+  //     })
+  // }
 
   $scope.markDrafted = function(){
-    $scope.drafted.push(this.player);
-    var ind = $scope.undrafted.indexOf(this.player);
+    $scope.drafted.push(this.player)
+    var ind = $scope.undrafted.indexOf(this.player)
     $scope.undrafted.splice(ind,1);
-    for(var key in this.player) {
+    for(key in this.player) {
       if (key !== "NAME" && key !== "Player") {
         $scope.teamStats[key] += parseInt(this.player[key]);
       }
@@ -95,26 +173,171 @@ angular.module('gDPopup', ['gDraft.services', 'angular-c3'])
         columns: [
           [
             'teamStats',
-            $scope.teamStats.MIN,
-            $scope.teamStats['3PM'],
-            $scope.teamStats['FG%'],
-            $scope.teamStats.BLK,
-            $scope.teamStats.STL,
-            $scope.teamStats.AST,
-            $scope.teamStats.GP,
-            $scope.teamStats.REB,
+            $scope.teamStats['FGM'],
+            $scope.teamStats['FGA'],
+            $scope.teamStats["FG%"],
+            $scope.teamStats['FTM'],
+            $scope.teamStats['FTA'],
             $scope.teamStats['FT%'],
-            $scope.teamStats.PTS,
-            $scope.teamStats.TO
-          ]
+            $scope.teamStats['3PTM'],
+            $scope.teamStats['PTS'],
+            $scope.teamStats['REB'],
+            $scope.teamStats['AST'],
+            $scope.teamStats['ST'],
+            $scope.teamStats['BLK'],
+            $scope.teamStats['TO'],
+          ],
         ],
       });
     });
 
-    $scope.calculate();
+    // $scope.calculate();
+  }
+
+  $scope.addPlayerStats = function(){
+    $scope.playerStats = this.player
+    console.log(this.player)
+    c3Factory.get('chart').then(function(chart) {
+      chart.load({
+        columns: [
+          [
+            'playerStats',
+            $scope.playerStats['FGM'],
+            $scope.playerStats['FGA'],
+            $scope.playerStats["FG%"],
+            $scope.playerStats['FTM'],
+            $scope.playerStats['FTA'],
+            $scope.playerStats['FT%'],
+            $scope.playerStats['3PTM'],
+            $scope.playerStats['PTS'],
+            $scope.playerStats['REB'],
+            $scope.playerStats['AST'],
+            $scope.playerStats['ST'],
+            $scope.playerStats['BLK'],
+            $scope.playerStats['TO'],
+          ]
+        ],
+      });
+    });
+  }
+
+  $scope.removePlayerStats = function(){
+    $scope.playerStats = {};
+    c3Factory.get('chart').then(function(chart) {
+      chart.unload({
+        ids:['playerStats']
+      });
+    });
+  }
+
+  // var svg = d3.select("body").append("svg")
+  //   .attr("width", "1000")
+  //   .attr("height", "1000")
+  //   .attr("fill", "blue")
+  //
+
+  // update: will be called on each ng-click or ng-mouseover event.
+  // should use the
+  // $scope.update = function() {
+  //   console.log('updating');
+  //   var data = [];
+  //   console.log(Object.keys($scope.teamStats));
+  //   for (key in $scope.teamStats) {
+  //     if (key !== '$$hashKey') {
+  //       data.push($scope.teamStats[key]);
+  //     }
+  //   };
+  //
+  //   console.log(data);
+  //
+  //   var graph = svg.selectAll("rect")
+  //     .data(data, function(d) {return d;})
+  //   console.log(graph);
+  //
+  //   graph.enter().append("rect")
+  //     .attr('width', function(d) { return d; })
+  //     .attr('height', 10)
+  //     .attr('fill','blue')
+  //     .attr('y', function(d, i) { return i * 10; })
+  // }
+
+  $scope.updateState = function() {
+
   };
 
-  $scope.init = function() {
-    parent.postMessage({command: 'init'}, '*')
-  };
+  window.addEventListener("message", receiveMessage, false);
+
+  function receiveMessage(event) {
+    console.log("=======================message received in slider.js!======================");
+    console.log(event.data);
+
+    if (event.data.user) {
+      $scope.user = event.data.user;
+    }
+
+    if (event.data.state) {
+      if (!$scope.user) {
+        console.log("error: user not set");
+      }
+      else {
+        // reset scope and teamStats
+        $scope.state = event.data.state;
+        $scope.teamStats = {
+          "FGM": 0,
+          "FGA": 0,
+          "FG%": 0,
+          "FTM": 0,
+          "FTA": 0,
+          "FT%": 0,
+          "3PTM": 0,
+          "PTS": 0,
+          "REB": 0,
+          "AST": 0,
+          "ST": 0,
+          "BLK": 0,
+          "TO": 0
+        };
+
+        var state = $scope.state;
+        var user = $scope.user;
+        console.log(user);
+        console.log(state[user]);
+        for (var key in state[user]) {
+          var player = state[user][key];
+          console.log(player);
+          for (var stat in player) {
+            var data = player[stat];
+            $scope.teamStats[stat] += parseFloat(data);
+          }
+        }
+        console.log($scope.teamStats);
+      }
+
+      // update angular bindings
+      $scope.$apply();
+
+      c3Factory.get('chart').then(function(chart) {
+        chart.load({
+          columns: [
+            [
+              'teamStats',
+              $scope.teamStats['FGM'],
+              $scope.teamStats['FGA'],
+              $scope.teamStats["FG%"],
+              $scope.teamStats['FTM'],
+              $scope.teamStats['FTA'],
+              $scope.teamStats['FT%'],
+              $scope.teamStats['3PTM'],
+              $scope.teamStats['PTS'],
+              $scope.teamStats['REB'],
+              $scope.teamStats['AST'],
+              $scope.teamStats['ST'],
+              $scope.teamStats['BLK'],
+              $scope.teamStats['TO'],
+            ],
+          ],
+        });
+      });
+    }
+  }
 });
